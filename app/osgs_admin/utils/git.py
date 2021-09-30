7cf573def3bbdeef1100e6b52518aa4f3beafe2c
Charlie <64078329+zacharlie@users.noreply.github.com>
2021-09-30 02:49:08 +0200

    add config menu
    update some git methods and add
    sidebar menu to config page

--- app/osgs_admin/utils/git.py
+++ app/osgs_admin/utils/git.py
@@ -1,10 +1,13 @@
 import git
 import json
+from shutil import rmtree
 from flask import current_app as app
 from ..models import Osgs
 
 osgs = Osgs.query.all()[0]
 osgs_repo = json.loads(osgs.config)["osgs_repo"]
+
+application_path = app.app_dir
 
 
 class Progress(git.remote.RemoteProgress):
@@ -12,4 +15,47 @@ class Progress(git.remote.RemoteProgress):
         print(self._cur_line)
 
 
-repo = git.Repo.clone_from(osgs_repo, app.app_dir, progress=Progress())
+def get_repo(repo_path: str = osgs_repo):
+    try:
+        repo = git.Repo(repo_path)
+        repo_issues = None
+    except InvalidGitRepositoryError as e:
+        repo = "Invalid Git Repository Specified"
+        repo_issues = [e]
+    except Exception as e:
+        repo = None
+        repo_issues = [e]
+    finally:
+        return repo, repo_issues
+
+
+def get_commit_hash(repo):
+    return repo.commit().hexsha
+
+
+def get_repo_info(repo):
+    heads = repo.heads
+    branches = [branch for branch.name in heads]
+    repo_info = {
+        "commit": get_commit_hash(repo),
+        "branch": repo.active_branch.name,
+        "branches": branches,
+    }
+    return repo_info
+
+
+def check_repo(repo):
+    """Do Some Things"""
+    pass
+
+
+def clone_repo(repo_path: str = osgs_repo):
+    repo = git.Repo.clone_from(repo_path, app.app_dir, progress=Progress())
+    return repo
+
+
+def destroy_repo(repo_path):
+    try:
+        rmtree(repo_path)
+    except:
+        raise DirectoryRemovalError
